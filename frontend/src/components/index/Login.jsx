9538fe7a600751581dd37f444c3fea7e48735d2e
JordanARS <jordanalejandrorodriguez@gmail.com>
2023-05-14 17:15:53 -0500

    Alerta de correo y supuesto validar si existe el correo o no

--- frontend/src/components/index/Login.jsx
+++ frontend/src/components/index/Login.jsx
@@ -10,6 +10,10 @@ const Login = () => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    if (!email || !password) {
+      alert("Por favor ingrese su correo y contraseña");
+      return;
+    }
     try {
       const response = await axios.post("http://127.0.0.1:8000/api/login/", {
         email,
@@ -20,119 +24,17 @@ const Login = () => {
       // redirigir a la página de inicio de sesión exitoso
       navigate("/registro");
     } catch (error) {
-      console.error(error);
+      //Para verificar si el correo no existe
+      if (error.response && error.response.status === 404) {
+        alert("El correo ingresado no existe");
+      } else {
+        console.error(error);
+      }
     }
   };
+  
 
   return (
-/*     <section>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div class="flex flex-col justify-center flex-1 px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
-          <div class="w-full max-w-xl mx-auto lg:w-96">
-            <div>
-              <h2 class="mt-6 text-3xl font-extrabold text-neutral-600">
-                Sign in.
-              </h2>
-            </div>
-
-            <div class="mt-8">
-              <div class="mt-6">
-                <form
-                  onSubmit={handleSubmit}
-                  action="#"
-                  method="POST"
-                  class="space-y-6"
-                >
-                  <div>
-                    <label
-                      for="email"
-                      class="block text-sm font-medium text-neutral-600"
-                    >
-                      {" "}
-                      Email address{" "}
-                    </label>
-                    <div class="mt-1">
-                      <input
-                        id="email"
-                        name="email"
-                        type="email"
-                        value={email}
-                        onChange={(event) => setEmail(event.target.value)}
-                        autocomplete="email"
-                        required=""
-                        placeholder="Your Email"
-                        class="block w-full px-5 py-3 text-base placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg text-neutral-600 bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300"
-                      />
-                    </div>
-                  </div>
-
-                  <div class="space-y-1">
-                    <label
-                      for="password"
-                      class="block text-sm font-medium text-neutral-600"
-                    >
-                      {" "}
-                      Password{" "}
-                    </label>
-                    <div class="mt-1">
-                      <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        value={password}
-                        onChange={(event) => setPassword(event.target.value)}
-                        autocomplete="current-password"
-                        required=""
-                        placeholder="Your Password"
-                        class="block w-full px-5 py-3 text-base placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg text-neutral-600 bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300"
-                      />
-                    </div>
-                  </div>
-
-                  <div class="flex items-center justify-between">
-                    <div class="flex items-center">
-                      <input
-                        id="remember-me"
-                        name="remember-me"
-                        type="checkbox"
-                        placeholder="Your password"
-                        class="w-4 h-4 text-blue-600 border-gray-200 rounded focus:ring-blue-500"
-                      />
-                      <label
-                        for="remember-me"
-                        class="block ml-2 text-sm text-neutral-600"
-                      >
-                        {" "}
-                        Remember me{" "}
-                      </label>
-                    </div>
-
-                    <div class="text-sm">
-                      <a
-                        href="#"
-                        class="font-medium text-blue-600 hover:text-blue-500"
-                      >
-                        {" "}
-                        Forgot your password?{" "}
-                      </a>
-                    </div>
-                  </div>
-
-                  <div>
-                    <button
-                      type="submit"
-                      class="flex items-center justify-center w-full px-10 py-4 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-blue-600 rounded-xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-                    >
-                      Sign in
-                    </button>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section> */
 
     <div className="flex flex-col lg:flex-row w-full h-screen">
   <div className="bg-[#111827] w-full lg:w-1/2 flex items-center justify-center">
@@ -179,6 +81,7 @@ const Login = () => {
 
         <div className='mt-6 flex flex-col gap-y-4'>
           <button
+            type="submit"
             className='active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-2 bg-[#55B7C9] font-semibold text-lg text-center rounded-xl'
           >
             Ingresar
@@ -189,7 +92,9 @@ const Login = () => {
           <p className='font-medium text-lg text-gray-200'>
             ¿No tienes cuenta?
           </p>
-          <button className='text-lg pl-3 text-[#55B7C9] hover:scale-[1.06] ease-in-out transition-all' href={"/registro"}>
+          <button 
+          
+          className='text-lg pl-3 text-[#55B7C9] hover:scale-[1.06] ease-in-out transition-all' onClick={() => navigate("/registro")}>
             Registrate
           </button>
         </div>
