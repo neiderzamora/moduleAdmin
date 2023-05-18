1fc7c1668688410dcaba97cbe51ce6fbf9d25d90
ihuntersouls <neiderzamoramoreno@gmail.com>
2023-05-18 10:21:42 -0500

    Diseño mejorado de pagina 404

--- frontend/src/components/index/Error.jsx
+++ frontend/src/components/index/Error.jsx
@@ -1,28 +1,37 @@
+import { Link } from "react-router-dom";
 const Error = () => {
   return (
     <>
-      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-        <div className="text-center">
-          <p className="text-base font-semibold text-indigo-600">404</p>
-          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-            Pagina no Existe
-          </h1>
-          <p className="mt-6 text-base leading-7 text-gray-600">
-          Lo sentimos, no pudimos encontrar la página que estás buscando
-          </p>
-          <div className="mt-10 flex items-center justify-center gap-x-6">
-            <a
-              href="/"
-              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Regresar al inicio
-            </a>
-            <a href="#" className="text-sm font-semibold text-gray-900">
-              Contactar Soporte <span aria-hidden="true">&rarr;</span>
-            </a>
+      <section className="w-full h-screen lg:p-100 bg-gray-900 " >
+        <div className="relative items-center w-full h-full px-5 py-12 mx-auto md:px-12 lg:px-16 max-w-7xl lg:py-24">
+          <div className="flex w-full h-full mx-auto text-left">
+            <div className="relative inline-flex items-center mx-auto align-middle">
+              <div className="text-center">
+              <h1 className="max-w-5xl inline bg-gradient-to-r from-indigo-200 via-sky-400 to-indigo-200 bg-clip-text text-transparent text-6xl md:text-7xl lg:text-9xl lg:max-w-7xl">
+                  404
+                </h1>
+                <h2 className="max-w-5xl text-5xl font-bold leading-none tracking-tighter text-neutral-300 md:text-5xl lg:text-6xl lg:max-w-7xl">
+                  Pagina no encontrada
+                </h2>
+                
+                <p className="max-w-xl mx-auto mt-8 text-base leading-relaxed text-gray-300">
+                La página que estas buscando pudo ser removida, sufrió un cambio de nombre o temporalmente no está disponible.
+                </p>
+                <div className="flex justify-center w-full max-w-2xl gap-2 mx-auto mt-6">
+                  <div className="mt-3 rounded-lg sm:mt-0">
+                    <Link to={"/"}> 
+                    <button className="items-center font-bold block px-5 py-4 text-base text-center text-white transition duration-500 ease-in-out transform bg-blue-600 lg:px-10 rounded-xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 animate-pulse" style={{ backgroundColor: '#0D0D0D', animationDuration: '3s' }}>
+                      Volver al inicio
+                    </button>
+                    </Link>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </main>
+      </section>
     </>
   );
 };
