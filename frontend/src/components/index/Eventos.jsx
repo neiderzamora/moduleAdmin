7da71eb5c9531b1ca0116c76afec065fa4ee1412
JordanARS <jordanalejandrorodriguez@gmail.com>
2023-05-18 11:04:53 -0500

    Jordan 11:04

--- frontend/src/components/index/Eventos.jsx
+++ frontend/src/components/index/Eventos.jsx
@@ -124,7 +124,7 @@ const Eventos = () => {
         <button
           type="submit"
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          onClick={() => navigate("/perfil")}
+          /* onClick={() => navigate("/perfil")} */
         >
           Enviar
         </button>
